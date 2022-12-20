0742ea069f625514cc990ba572e1e443afc45ef4
alfiusstevanus <88620306+alfiusstevanus@users.noreply.github.com>
2022-12-20 11:59:18 +0700

    Update PersegiPanjang.java

--- APPBangunDatar/src/main/java/com/mycompany/appbangundatar/PersegiPanjang.java
+++ APPBangunDatar/src/main/java/com/mycompany/appbangundatar/PersegiPanjang.java
@@ -1,13 +1,42 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package com.mycompany.appbangundatar;
 
-/**
- *
- * @author ramdhan
- */
-public class PersegiPanjang {
+public class PersegiPanjang extends BangunDatar{
     
+    private double panjang;
+    private double lebar;
+
+    public PersegiPanjang() {
+    }
+
+    public PersegiPanjang(String namaBangunDatar) {
+        super(namaBangunDatar);
+    }
+
+    public double getPanjang() {
+        return panjang;
+    }
+
+    public void setPanjang(double panjang) {
+        this.panjang = panjang;
+    }
+
+    public double getLebar() {
+        return lebar;
+    }
+
+    public void setLebar(double lebar) {
+        this.lebar = lebar;
+    }
+    
+    @Override
+    double luas() {
+        return panjang * lebar;
+    }
+
+    @Override
+    double keliling() {
+        return 2 * (panjang + lebar);
+    }
+    
+    
 }
